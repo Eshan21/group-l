6c0006e211ebdc8f9515873684ad980cf0f66148
Ishan Kapoor <falcon.kapoor@gmail.com>
2021-11-18 09:31:36 -0800

    concern* I can't spell

--- src/main/java/com/group-l/ricelog/Main.java
+++ src/main/java/com/group-l/ricelog/Main.java
@@ -9,6 +9,6 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-    System.out.println("Michael is TKperson");
+    System.out.println("[Exposed] Michael is TKperson");
 
 }
