e3be6187bdacdf751a9bed90d175f320df3e87e7
Akhil Guntur <55261146+algolg@users.noreply.github.com>
2021-11-18 10:06:23 -0800

    bird page works  - controller didn't have @Controller

--- src/main/java/com/group/controllers/Birds.java
+++ src/main/java/com/group/controllers/Birds.java
@@ -3,6 +3,7 @@ package com.group.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class Birds {
     @GetMapping("/birds")
     public String birds() {
