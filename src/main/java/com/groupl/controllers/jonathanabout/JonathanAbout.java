801a3762bdd52db02edc8bd0fc8f6a81f94c4643
jlaaa <jonathanylin1111@gmail.com>
2021-12-06 21:47:43 -0800

    Add FRQ to Jonathan About Page

--- src/main/java/com/groupl/controllers/jonathanabout/JonathanAbout.java
+++ src/main/java/com/groupl/controllers/jonathanabout/JonathanAbout.java
@@ -1,13 +1,15 @@
-package com.groupl.controllers.about;
+package com.groupl.controllers.jonathanabout;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class JonathanAbout {
-    @GetMapping("/about/jonathanabout");
+    @GetMapping("/about/jonathanabout")
     public String jonathanabout() {
-        return "jonathanabout";
+
+        return "/about/jonathanabout";
+
     }
 }
 
