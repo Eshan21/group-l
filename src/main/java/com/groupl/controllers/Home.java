d883f16af4448157250d1e90bcc8fe941dd07d06
jlaaa <jonathanylin1111@gmail.com>
2021-12-09 21:12:21 -0800

    Create new home page template

---
+++ src/main/java/com/groupl/controllers/Home.java
@@ -0,0 +1,13 @@
+package com.groupl.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+    @GetMapping("/")
+    public String Home() {
+        return "home";
+    }
+}
+
