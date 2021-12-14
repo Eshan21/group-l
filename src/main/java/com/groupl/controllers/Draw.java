764346980ed33646982997b5617e55bb3f8d5e7d
Ishan Kapoor <ikapoor@ucdavis.edu>
2021-12-14 10:45:56 -0800

    i did a thing pt. 103

--- src/main/java/com/groupl/controllers/Draw.java
+++ src/main/java/com/groupl/controllers/Draw.java
@@ -1,7 +1,9 @@
 package com.groupl.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class Draw {
     @GetMapping("/draw")   // GET request
     public String draw() {
