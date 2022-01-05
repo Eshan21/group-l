6cbe26d7fb6716c5c78ee386db677f04e7a22b9f
Ishan Kapoor <ikapoor@ucdavis.edu>
2022-01-05 09:45:38 -0800

    i did a thing pt. 109 (FRQ4 Edition)

--- src/main/java/com/groupl/controllers/ishanabout/IshanAbout.java
+++ src/main/java/com/groupl/controllers/ishanabout/IshanAbout.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Scanner;
+
 @Controller
 public class IshanAbout {
     @GetMapping("/about/ishanabout")
@@ -29,5 +31,38 @@ public class IshanAbout {
         model.addAttribute("newSeq", newSeq);
         return "about/ishanabout";
     }
+
+    public static String Frq4() {
+        Scanner s = new Scanner(System.in);
+            System.out.println("Enter input: ");
+            String input = s.nextLine();
+
+            int startInd = 0, endInd = 0, streak = 0, highStreak = 0, fsI = 0, feI = 0;
+            char character = 'x';
+            for (int i = 1; i < input.length(); i++) {
+                if (input.charAt(i) == input.charAt(i-1)) {
+                    streak++;
+                    if (i+1 == input.length() ) {
+                        endInd = i;
+                        highStreak = streak;
+                        character = input.charAt(i);
+                        fsI = startInd;
+                    }
+                } else {
+
+                    if (streak > highStreak){
+                        highStreak = streak;
+                        streak = 0;
+                        character = input.charAt(i-1);
+                        fsI = startInd;
+                        feI = endInd;
+                    } else streak = 0;
+                    startInd = i;
+                    endInd = i-1;
+                }
+            }
+            return "start,end,streak,char: " + fsI + "," + feI + "," + highStreak + "," + character;
+    }
+
 }
 
